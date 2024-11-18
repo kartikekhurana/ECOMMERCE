70fb585b30a2130cde8a904885af9efcc9c9f479
kartikekhurana <kartikekhurana@gmail.com>
2024-11-18 14:20:58 +0530

    created diffrent pages for website and their routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import "./App.css";
-import Layout from "./components/layout/layout";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/homePage.js";
+import About from "./pages/About.js";
+import Contact from "./pages/Contact.js";
+import Policy from "./pages/Policy.js";
+import PAgeNotFound from "./pages/Pagenotfound.js";
 function App() {
   return (
     <>
-      <Layout>
-        <h1>Ecommerce app</h1>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="about" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/*" element={<PAgeNotFound />} />
+      </Routes>
     </>
   );
 }
---
+++ client/src/pages/Policy.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../components/layout/layout.js";
+const policy = () => {
+  return (
+    <Layout>
+      <h1>Privacy Policy Page</h1>
+    </Layout>
+  );
+};
+
+export default policy;
